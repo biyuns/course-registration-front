7844af5c1d9b7dad332a148b3b589ea5e1362556
biyuns <cart0089@naver.com>
2025-12-06 03:12:44 +0900

    로그인 화면 구현, 웹 버전 사이드바 구현, 웹 버전 상단바 구현,  Route 설정

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,47 @@
+import { Outlet, NavLink } from 'react-router-dom'
+import '../css/Sidebar.css'
+
+import SideHome from '../img/side-home-img.svg';
+import SideSchedule from '../img/side-schedule-img.svg'
+import SideManager from '../img/side-manager-img.svg'
+import SideUser from '../img/side-user-img.svg'
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar-ct">
+            
+            <aside className='side-ct'>
+                
+                <section className='side-content-ct'>
+
+                    <div className='side-home'> 
+                        <img src={SideHome} alt="사이드 홈 이미지"/>
+                        <p> 홈</p>
+                    </div>
+
+                    <div className='side-schedule'>
+                        <img src={SideSchedule} alt="사이드 일정 이미지"/>
+                        <p> 일정 </p>
+                    </div>
+
+                    <div className='side-manager'>
+                        <img src={SideManager} alt="사이드 관리자 이미지"/>
+                        <p> 관리자</p>
+                    </div>
+
+                </section>
+
+                <div className='side-user'>
+                    <img src={SideUser} alt="사이드 유저 이미지"/>
+                </div>
+
+            </aside>
+        <main>
+            <Outlet />            
+        </main>
+
+        </div>
+    )
+}
+
+export default Sidebar;
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,33 @@
+import '../css/SignUp.css';
+import GoogleLoginimg from '../img/google-login-logo.svg';
+import NaverLoginimg from '../img/naver-login-log.png';
+import Header from './Header';
+
+
+export default function SignUp() {
+    return (
+        <div className='media-ct'>
+            <Header />
+            <section className='login-ct'>
+                <p> 로그인 </p>
+            
+                <input type="text" className='id-input' placeholder='아이디' />
+                <input type="password" className='password-input' placeholder='비밀번호' />
+                <button className="login-btn"> 로그인 </button>
+
+                <div className='find-singup-ct'>
+                    <p className='id-find'> 아이디 찾기 </p>
+                    <div> | </div>
+                    <p className='password-find'> 비밀번호 찾기 </p>
+                    <div> | </div>
+                    <p className='signup'> 회원가입 </p>
+                </div>
+                
+                <div className='google-naver-login'>
+                    <img className="google-login-logo"src={GoogleLoginimg} alt="구글 로그인 로고" />   
+                    <img className="naver-login-logo" src={NaverLoginimg} alt="네이버 로그인 로고" />
+                </div>
+            </section>
+        </div>
+    )
+}
