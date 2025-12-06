12db9357f5f5b598f5b54ad0e555234cca6f163d
sieun2im <kse040531@naver.com>
2025-12-07 04:23:52 +0900

    home

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import './css/reset.css'
-
 import Sidebar from './components/Sidebar.jsx'
 import Login from './pages/Login.jsx';
 import SignUp from './pages/SignUp.jsx';
 import SignUpNormal from './pages/LocalSignUp.jsx';
+import Home from './pages/Home.jsx';
+import ClassApply from './pages/ClassApply.jsx';
 
 function App() {
   return (
@@ -15,9 +16,11 @@ function App() {
       <Routes>
 
         <Route element={<Sidebar />}>
-          <Route path="/" element={<Login />}/>
-          <Route path="/signup" element={<SignUp />}/>
-          <Route path="/signup-normal" element={<SignUpNormal />}/>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signup-normal" element={<SignUpNormal />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/apply" element={<ClassApply />} />
         </Route>
 
       </Routes>
